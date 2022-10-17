6ac64f1a023e6ffc75f7ac9d7380e6703946e440
Junior Paula <luke.junnior@icloud.com>
2022-10-17 09:07:51 -0300

    add husky and jest configurate

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,10 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
   },
   rules: {},
 };
