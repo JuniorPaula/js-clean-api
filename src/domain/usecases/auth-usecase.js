54afdbacadc8c6224085df728746c3d31c58d01e
Junior Paula <luke.junnior@icloud.com>
2022-10-25 13:51:11 -0300

    move AuthUsecase to its onwer file

---
+++ src/domain/usecases/auth-usecase.js
@@ -0,0 +1,30 @@
+const { MissingParamError, InvalidParamError } = require('../../utils/errors');
+
+class AuthUsecase {
+  constructor(loadUserByEmailRepository) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository;
+  }
+
+  async auth(email, password) {
+    if (!email) {
+      throw new MissingParamError('email');
+    }
+    if (!password) {
+      throw new MissingParamError('password');
+    }
+    if (!this.loadUserByEmailRepository) {
+      throw new MissingParamError('loadUserByEmailRepository');
+    }
+    if (!this.loadUserByEmailRepository.load) {
+      throw new InvalidParamError('loadUserByEmailRepository');
+    }
+
+    const user = await this.loadUserByEmailRepository.load(email);
+
+    if (!user) {
+      return null;
+    }
+  }
+}
+
+module.exports = { AuthUsecase };
