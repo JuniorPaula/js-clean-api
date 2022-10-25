b3b70c45dae868bfd9b8760286892a7219d32b47
Junior Paula <luke.junnior@icloud.com>
2022-10-25 07:20:07 -0300

    move sut creation to a factory method

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -17,30 +17,39 @@ class AuthUsecase {
   }
 }
 
+const makeSut = () => {
+  class LoadUserByEmailRepositoryStub {
+    async load(email) {
+      this.email = email;
+    }
+  }
+
+  const loadUserByEmailRepositoryStub = new LoadUserByEmailRepositoryStub();
+  const sut = new AuthUsecase(loadUserByEmailRepositoryStub);
+
+  return {
+    sut,
+    loadUserByEmailRepositoryStub,
+  };
+};
+
 describe('AuthUsecase', () => {
   test('Should throw if no email is provided', async () => {
-    const sut = new AuthUsecase();
+    const { sut } = makeSut();
     const promise = sut.auth();
 
     await expect(promise).rejects.toThrow(new MissingParamError('email'));
   });
 
   test('Should throw if no password is provided', async () => {
-    const sut = new AuthUsecase();
+    const { sut } = makeSut();
     const promise = sut.auth('any_email@mail.com');
 
     await expect(promise).rejects.toThrow(new MissingParamError('password'));
   });
 
   test('Should call loadUserByEmail with correct email', async () => {
-    class LoadUserByEmailRepositoryStub {
-      async load(email) {
-        this.email = email;
-      }
-    }
-
-    const loadUserByEmailRepositoryStub = new LoadUserByEmailRepositoryStub();
-    const sut = new AuthUsecase(loadUserByEmailRepositoryStub);
+    const { sut, loadUserByEmailRepositoryStub } = makeSut();
     await sut.auth('any_email@mail.com', 'any_password');
 
     expect(loadUserByEmailRepositoryStub.email).toBe('any_email@mail.com');
