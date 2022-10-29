902054609acca31eccbafe3b62f891c39e6112cf
Junior Paula <luke.junnior@icloud.com>
2022-10-29 17:27:38 -0300

    ensure request return content type xml it forced

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -9,4 +9,15 @@ describe('#ContentType Middleware', () => {
 
     await request(app).get('/test_content_type').expect('content-type', /json/);
   });
+
+  test('Should return #xml content-type it forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml');
+      res.send('');
+    });
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/);
+  });
 });
