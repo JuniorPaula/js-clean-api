90d5bd735376b49e37cb610c32bd6cba6ed02532
Junior Paula <luke.junnior@icloud.com>
2022-10-29 15:46:28 -0300

    move UpdateAccessTokenRepository to its own file

--- src/infra/repositories/update-accesstoken-repository.spec.js
+++ src/infra/repositories/update-accesstoken-repository.spec.js
@@ -1,33 +1,10 @@
 const { MissingParamError } = require('../../utils/errors');
 const { MongoHelper } = require('../helpers/mongodb-helper');
+const {
+  UpdateAccessTokenRepository,
+} = require('./update-accesstoken-repository');
+
 let db;
-
-class UpdateAccessTokenRepository {
-  constructor(userModel) {
-    this.userModel = userModel;
-  }
-
-  async update(userId, accessToken) {
-    if (!userId) {
-      throw new MissingParamError('userId');
-    }
-
-    if (!accessToken) {
-      throw new MissingParamError('accessToken');
-    }
-
-    await this.userModel.updateOne(
-      {
-        _id: userId,
-      },
-      {
-        $set: {
-          accessToken,
-        },
-      },
-    );
-  }
-}
 
 const makeSut = () => {
   const userModel = db.collection('users');
