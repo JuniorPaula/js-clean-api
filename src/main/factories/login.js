04af67878c1f514d9fb879bc7afe965b73f8a780
Junior Paula <luke.junnior@icloud.com>
2022-11-05 11:07:27 -0300

    create loginController factory

---
+++ src/main/factories/login.js
@@ -0,0 +1,30 @@
+const env = require('../config/env');
+const { LoginController } = require('../../aplication/controllers/login/login');
+const { AuthUsecase } = require('../../domain/usecases/auth-usecase');
+const { EmailValidator } = require('../../utils/validations/email-validator');
+const {
+  LoadUserByEmailRepository,
+} = require('../../infra/repositories/load-user-by-email-repository');
+const {
+  UpdateAccessTokenRepository,
+} = require('../../infra/repositories/update-accesstoken-repository');
+const { Encrypter } = require('../../infra/criptography/encrypter');
+const { TokenGenerator } = require('../../infra/criptography/token-generator');
+
+const makeLoginController = () => {
+  const loadUserByEmailRepository = new LoadUserByEmailRepository();
+  const encrypter = new Encrypter();
+  const tokenGenerator = new TokenGenerator(env.tokeSecret);
+  const updateAccessTokenRepository = new UpdateAccessTokenRepository();
+
+  const emailValidator = new EmailValidator();
+  const authUsecase = new AuthUsecase(
+    loadUserByEmailRepository,
+    encrypter,
+    tokenGenerator,
+    updateAccessTokenRepository,
+  );
+  return new LoginController(authUsecase, emailValidator);
+};
+
+module.exports = { makeLoginController };
--- src/main/routes/login-routes.js
+++ src/main/routes/login-routes.js
@@ -1,32 +1,8 @@
-const env = require('../config/env');
-const { LoginController } = require('../../aplication/controllers/login/login');
-const { AuthUsecase } = require('../../domain/usecases/auth-usecase');
-const { EmailValidator } = require('../../utils/validations/email-validator');
-const {
-  LoadUserByEmailRepository,
-} = require('../../infra/repositories/load-user-by-email-repository');
-const {
-  UpdateAccessTokenRepository,
-} = require('../../infra/repositories/update-accesstoken-repository');
-const { Encrypter } = require('../../infra/criptography/encrypter');
-const { TokenGenerator } = require('../../infra/criptography/token-generator');
+const { adapterRoutes } = require('../adapters/express-routes-adapter');
+const { makeLoginController } = require('../factories/login');
 
 const loginRouter = (router) => {
-  const loadUserByEmailRepository = new LoadUserByEmailRepository();
-  const encrypter = new Encrypter();
-  const tokenGenerator = new TokenGenerator(env.tokeSecret);
-  const updateAccessTokenRepository = new UpdateAccessTokenRepository();
-
-  const emailValidator = new EmailValidator();
-  const authUsecase = new AuthUsecase(
-    loadUserByEmailRepository,
-    encrypter,
-    tokenGenerator,
-    updateAccessTokenRepository,
-  );
-  const loginController = new LoginController(authUsecase, emailValidator);
-
-  router.post('/login', loginController);
+  router.post('/login', adapterRoutes(makeLoginController()));
 };
 
 module.exports = loginRouter;
