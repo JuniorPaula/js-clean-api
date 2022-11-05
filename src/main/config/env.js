ff86a4dbf923e65b60ab4f89465f376af6a4ddb0
Junior Paula <luke.junnior@icloud.com>
2022-11-05 10:52:48 -0300

    create loginRouter

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongodbUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-js-api',
   port: process.env.PORT || 3033,
+  tokeSecret: process.env.TOKEN_SECRET || 'sdj38db827t45kjsbd$%asd31247$%#gfsg',
 };
--- src/main/routes/login-routes.js
+++ src/main/routes/login-routes.js
@@ -1,5 +1,32 @@
-const loginRouter = () => {
-  // router.post('/login', (req, res) => {});
+const env = require('../config/env');
+const { LoginController } = require('../../aplication/controllers/login/login');
+const { AuthUsecase } = require('../../domain/usecases/auth-usecase');
+const { EmailValidator } = require('../../utils/validations/email-validator');
+const {
+  LoadUserByEmailRepository,
+} = require('../../infra/repositories/load-user-by-email-repository');
+const {
+  UpdateAccessTokenRepository,
+} = require('../../infra/repositories/update-accesstoken-repository');
+const { Encrypter } = require('../../infra/criptography/encrypter');
+const { TokenGenerator } = require('../../infra/criptography/token-generator');
+
+const loginRouter = (router) => {
+  const loadUserByEmailRepository = new LoadUserByEmailRepository();
+  const encrypter = new Encrypter();
+  const tokenGenerator = new TokenGenerator(env.tokeSecret);
+  const updateAccessTokenRepository = new UpdateAccessTokenRepository();
+
+  const emailValidator = new EmailValidator();
+  const authUsecase = new AuthUsecase(
+    loadUserByEmailRepository,
+    encrypter,
+    tokenGenerator,
+    updateAccessTokenRepository,
+  );
+  const loginController = new LoginController(authUsecase, emailValidator);
+
+  router.post('/login', loginController);
 };
 
 module.exports = loginRouter;
