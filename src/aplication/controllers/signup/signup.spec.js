cca3d58c55a634073bb766c45712290d97f392db
Junior Paula <luke.junnior@icloud.com>
2022-11-07 18:59:19 -0300

    ensure SignupController return 500 if no createAccount is provided

--- src/aplication/controllers/signup/signup.spec.js
+++ src/aplication/controllers/signup/signup.spec.js
@@ -158,4 +158,20 @@ describe('Signup Controller', () => {
     expect(createAccountSpy.email).toBe(httpRequest.body.email);
     expect(createAccountSpy.password).toBe(httpRequest.body.password);
   });
+
+  test('Should return 500 if no createAccount is provided', async () => {
+    const sut = new SignupController();
+    const httpRequest = {
+      body: {
+        username: 'any_username',
+        email: 'any_mail@mail.com',
+        password: '1234',
+        confirmPassword: '1234',
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body.error).toBe(new ServerError().message);
+  });
 });
