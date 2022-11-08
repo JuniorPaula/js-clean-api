6e1983bc01b793629c344a0f860aa9031e33ea26
Junior Paula <luke.junnior@icloud.com>
2022-11-08 20:00:53 -0300

    move dependencies tests to owner describes

--- src/aplication/controllers/signup/signup.spec.js
+++ src/aplication/controllers/signup/signup.spec.js
@@ -101,270 +101,280 @@ const makeSut = () => {
 };
 
 describe('Signup Controller', () => {
-  test('Should return 500 if no HttpRequest is provided', async () => {
-    const { sut } = makeSut();
+  describe('HttpRequest params', () => {
+    test('Should return 500 if no HttpRequest is provided', async () => {
+      const { sut } = makeSut();
 
-    const httpResponse = await sut.handle();
-    expect(httpResponse.statusCode).toBe(500);
-    expect(httpResponse.body.error).toBe(new ServerError().message);
-  });
+      const httpResponse = await sut.handle();
+      expect(httpResponse.statusCode).toBe(500);
+      expect(httpResponse.body.error).toBe(new ServerError().message);
+    });
 
-  test('Should return 500 if HttpRequest has no body', async () => {
-    const { sut } = makeSut();
-    const httpRequest = {};
+    test('Should return 500 if HttpRequest has no body', async () => {
+      const { sut } = makeSut();
+      const httpRequest = {};
 
-    const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(500);
-    expect(httpResponse.body.error).toBe(new ServerError().message);
-  });
+      const httpResponse = await sut.handle(httpRequest);
+      expect(httpResponse.statusCode).toBe(500);
+      expect(httpResponse.body.error).toBe(new ServerError().message);
+    });
 
-  test('Should call createAccount with correct values', async () => {
-    const { sut, createAccountSpy } = makeSut();
-    const httpRequest = {
-      body: {
-        username: 'any_username',
-        email: 'any_mail@mail.com',
-        password: '1234',
-        confirmPassword: '1234',
-      },
-    };
+    test('Should return 200 valid data are provided', async () => {
+      const { sut } = makeSut();
+      const httpRequest = {
+        body: {
+          username: 'valid_username',
+          email: 'valid_mail@mail.com',
+          password: '1234',
+          confirmPassword: '1234',
+        },
+      };
 
-    await sut.handle(httpRequest);
-    expect(createAccountSpy.username).toBe(httpRequest.body.username);
-    expect(createAccountSpy.email).toBe(httpRequest.body.email);
-    expect(createAccountSpy.password).toBe(httpRequest.body.password);
-  });
-
-  test('Should return 500 if no CreateAccount is provided', async () => {
-    const sut = new SignupController();
-    const httpRequest = {
-      body: {
-        username: 'any_username',
-        email: 'any_mail@mail.com',
-        password: '1234',
-        confirmPassword: '1234',
-      },
-    };
-
-    const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(500);
-    expect(httpResponse.body.error).toBe(new ServerError().message);
-  });
-
-  test('Should return 500 if CreateAccount has no method create', async () => {
-    class CreateAccountSpy {}
-    const createAccountSpy = new CreateAccountSpy();
-    const sut = new SignupController(createAccountSpy);
-    const httpRequest = {
-      body: {
-        username: 'any_username',
-        email: 'any_mail@mail.com',
-        password: '1234',
-        confirmPassword: '1234',
-      },
-    };
-
-    const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(500);
-    expect(httpResponse.body.error).toBe(new ServerError().message);
-  });
-
-  test('Should return 500 if CreateAccount throws', async () => {
-    const createAccountSpyError = mockCreateAccountSpyError();
-    const sut = new SignupController(createAccountSpyError);
-
-    const httpRequest = {
-      body: {
-        username: 'any_username',
-        email: 'any_mail@mail.com',
-        password: '1234',
-        confirmPassword: '1234',
-      },
-    };
-
-    const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(500);
-    expect(httpResponse.body.error).toEqual(new ServerError().message);
-  });
-
-  test('Should return 403 if CreateAccount returns null', async () => {
-    const { sut, createAccountSpy } = makeSut();
-
-    jest.spyOn(createAccountSpy, 'create').mockReturnValueOnce(null);
-
-    const httpRequest = {
-      body: {
-        username: 'any_username',
-        email: 'any_mail@mail.com',
-        password: '1234',
-        confirmPassword: '1234',
-      },
-    };
-
-    const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse.body.error).toBe(new EmailAlreadyExists().message);
-  });
-
-  test('Should call AuthenticationUsecase with correct params', async () => {
-    const { sut, authenticationUsecaseSpy } = makeSut();
-    const httpRequest = {
-      body: {
-        username: 'any_username',
-        email: 'any_mail@mail.com',
-        password: '1234',
-        confirmPassword: '1234',
-      },
-    };
-
-    await sut.handle(httpRequest);
-    expect(authenticationUsecaseSpy.email).toBe(httpRequest.body.email);
-    expect(authenticationUsecaseSpy.password).toBe(httpRequest.body.password);
-  });
-
-  test('Should return 500 if no AuthenticationUsecase is provided', async () => {
-    const sut = new SignupController();
-    const httpRequest = {
-      body: {
-        username: 'any_username',
-        email: 'any_mail@mail.com',
-        password: '1234',
-        confirmPassword: '1234',
-      },
-    };
-
-    const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(500);
-    expect(httpResponse.body.error).toBe(new ServerError().message);
-  });
-
-  test('Should return 500 if AuthenticationUsecase has no method create', async () => {
-    class AuthenticationUsecaseSpy {}
-    const authenticationUsecase = new AuthenticationUsecaseSpy();
-    const sut = new SignupController(authenticationUsecase);
-    const httpRequest = {
-      body: {
-        username: 'any_username',
-        email: 'any_mail@mail.com',
-        password: '1234',
-        confirmPassword: '1234',
-      },
-    };
-
-    const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(500);
-    expect(httpResponse.body.error).toBe(new ServerError().message);
-  });
-
-  test('Should return 500 if AuthenticationUsecase throws', async () => {
-    const authenticationUsecaseSpy = mockauthenticationUsecaseSpyError();
-    const sut = new SignupController(authenticationUsecaseSpy);
-
-    const httpRequest = {
-      body: {
-        username: 'any_username',
-        email: 'any_mail@mail.com',
-        password: '1234',
-        confirmPassword: '1234',
-      },
-    };
-
-    const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(500);
-    expect(httpResponse.body.error).toEqual(new ServerError().message);
-  });
-
-  test('Should return 200 valid data are provided', async () => {
-    const { sut } = makeSut();
-    const httpRequest = {
-      body: {
+      const httpResponse = await sut.handle(httpRequest);
+      expect(httpResponse.statusCode).toBe(200);
+      expect(httpResponse.body).toEqual({
+        access_token: 'valid_accessToken',
         username: 'valid_username',
-        email: 'valid_mail@mail.com',
-        password: '1234',
-        confirmPassword: '1234',
-      },
-    };
-
-    const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(200);
-    expect(httpResponse.body).toEqual({
-      access_token: 'valid_accessToken',
-      username: 'valid_username',
+      });
     });
   });
 
-  test('Should call Validator with correct values', async () => {
-    const { sut, validatorSpy } = makeSut();
-    const httpRequest = {
-      body: {
-        username: 'valid_username',
-        email: 'valid_mail@mail.com',
-        password: '1234',
-        confirmPassword: '1234',
-      },
-    };
+  describe('CreateAccount', () => {
+    test('Should call createAccount with correct values', async () => {
+      const { sut, createAccountSpy } = makeSut();
+      const httpRequest = {
+        body: {
+          username: 'any_username',
+          email: 'any_mail@mail.com',
+          password: '1234',
+          confirmPassword: '1234',
+        },
+      };
 
-    await sut.handle(httpRequest);
+      await sut.handle(httpRequest);
+      expect(createAccountSpy.username).toBe(httpRequest.body.username);
+      expect(createAccountSpy.email).toBe(httpRequest.body.email);
+      expect(createAccountSpy.password).toBe(httpRequest.body.password);
+    });
 
-    expect(validatorSpy.username).toBe(httpRequest.body.username);
-    expect(validatorSpy.email).toBe(httpRequest.body.email);
-    expect(validatorSpy.password).toBe(httpRequest.body.password);
-    expect(validatorSpy.confirmPassword).toBe(httpRequest.body.confirmPassword);
+    test('Should return 500 if no CreateAccount is provided', async () => {
+      const sut = new SignupController();
+      const httpRequest = {
+        body: {
+          username: 'any_username',
+          email: 'any_mail@mail.com',
+          password: '1234',
+          confirmPassword: '1234',
+        },
+      };
+
+      const httpResponse = await sut.handle(httpRequest);
+      expect(httpResponse.statusCode).toBe(500);
+      expect(httpResponse.body.error).toBe(new ServerError().message);
+    });
+
+    test('Should return 500 if CreateAccount has no method create', async () => {
+      class CreateAccountSpy {}
+      const createAccountSpy = new CreateAccountSpy();
+      const sut = new SignupController(createAccountSpy);
+      const httpRequest = {
+        body: {
+          username: 'any_username',
+          email: 'any_mail@mail.com',
+          password: '1234',
+          confirmPassword: '1234',
+        },
+      };
+
+      const httpResponse = await sut.handle(httpRequest);
+      expect(httpResponse.statusCode).toBe(500);
+      expect(httpResponse.body.error).toBe(new ServerError().message);
+    });
+
+    test('Should return 500 if CreateAccount throws', async () => {
+      const createAccountSpyError = mockCreateAccountSpyError();
+      const sut = new SignupController(createAccountSpyError);
+
+      const httpRequest = {
+        body: {
+          username: 'any_username',
+          email: 'any_mail@mail.com',
+          password: '1234',
+          confirmPassword: '1234',
+        },
+      };
+
+      const httpResponse = await sut.handle(httpRequest);
+      expect(httpResponse.statusCode).toBe(500);
+      expect(httpResponse.body.error).toEqual(new ServerError().message);
+    });
+
+    test('Should return 403 if CreateAccount returns null', async () => {
+      const { sut, createAccountSpy } = makeSut();
+
+      jest.spyOn(createAccountSpy, 'create').mockReturnValueOnce(null);
+
+      const httpRequest = {
+        body: {
+          username: 'any_username',
+          email: 'any_mail@mail.com',
+          password: '1234',
+          confirmPassword: '1234',
+        },
+      };
+
+      const httpResponse = await sut.handle(httpRequest);
+      expect(httpResponse.body.error).toBe(new EmailAlreadyExists().message);
+    });
   });
 
-  test('Should return 400 Validator return an error', async () => {
-    const { sut, validatorSpy } = makeSut();
-    jest
-      .spyOn(validatorSpy, 'validate')
-      .mockReturnValueOnce(new Error('any_field').message);
+  describe('AuthenticationUsecase', () => {
+    test('Should call AuthenticationUsecase with correct params', async () => {
+      const { sut, authenticationUsecaseSpy } = makeSut();
+      const httpRequest = {
+        body: {
+          username: 'any_username',
+          email: 'any_mail@mail.com',
+          password: '1234',
+          confirmPassword: '1234',
+        },
+      };
 
-    const httpRequest = {
-      body: {
-        username: 'valid_username',
-        email: 'valid_mail@mail.com',
-        password: '1234',
-        confirmPassword: '1234',
-      },
-    };
+      await sut.handle(httpRequest);
+      expect(authenticationUsecaseSpy.email).toBe(httpRequest.body.email);
+      expect(authenticationUsecaseSpy.password).toBe(httpRequest.body.password);
+    });
 
-    const httpResponse = await sut.handle(httpRequest);
+    test('Should return 500 if no AuthenticationUsecase is provided', async () => {
+      const sut = new SignupController();
+      const httpRequest = {
+        body: {
+          username: 'any_username',
+          email: 'any_mail@mail.com',
+          password: '1234',
+          confirmPassword: '1234',
+        },
+      };
 
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body.error).toBe(
-      new MissingParamError('any_field').message,
-    );
+      const httpResponse = await sut.handle(httpRequest);
+      expect(httpResponse.statusCode).toBe(500);
+      expect(httpResponse.body.error).toBe(new ServerError().message);
+    });
+
+    test('Should return 500 if AuthenticationUsecase has no method create', async () => {
+      class AuthenticationUsecaseSpy {}
+      const authenticationUsecase = new AuthenticationUsecaseSpy();
+      const sut = new SignupController(authenticationUsecase);
+      const httpRequest = {
+        body: {
+          username: 'any_username',
+          email: 'any_mail@mail.com',
+          password: '1234',
+          confirmPassword: '1234',
+        },
+      };
+
+      const httpResponse = await sut.handle(httpRequest);
+      expect(httpResponse.statusCode).toBe(500);
+      expect(httpResponse.body.error).toBe(new ServerError().message);
+    });
+
+    test('Should return 500 if AuthenticationUsecase throws', async () => {
+      const authenticationUsecaseSpy = mockauthenticationUsecaseSpyError();
+      const sut = new SignupController(authenticationUsecaseSpy);
+
+      const httpRequest = {
+        body: {
+          username: 'any_username',
+          email: 'any_mail@mail.com',
+          password: '1234',
+          confirmPassword: '1234',
+        },
+      };
+
+      const httpResponse = await sut.handle(httpRequest);
+      expect(httpResponse.statusCode).toBe(500);
+      expect(httpResponse.body.error).toEqual(new ServerError().message);
+    });
   });
 
-  test('Should return 500 if no Validator is provided', async () => {
-    const sut = new SignupController();
-    const httpRequest = {
-      body: {
-        username: 'any_username',
-        email: 'any_mail@mail.com',
-        password: '1234',
-        confirmPassword: '1234',
-      },
-    };
+  describe('Validator', () => {
+    test('Should call Validator with correct values', async () => {
+      const { sut, validatorSpy } = makeSut();
+      const httpRequest = {
+        body: {
+          username: 'valid_username',
+          email: 'valid_mail@mail.com',
+          password: '1234',
+          confirmPassword: '1234',
+        },
+      };
 
-    const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(500);
-    expect(httpResponse.body.error).toBe(new ServerError().message);
-  });
+      await sut.handle(httpRequest);
 
-  test('Should return 500 if Validator has no method create', async () => {
-    class ValidatorSpy {}
-    const validatorSpy = new ValidatorSpy();
-    const sut = new SignupController(validatorSpy);
-    const httpRequest = {
-      body: {
-        username: 'any_username',
-        email: 'any_mail@mail.com',
-        password: '1234',
-        confirmPassword: '1234',
-      },
-    };
+      expect(validatorSpy.username).toBe(httpRequest.body.username);
+      expect(validatorSpy.email).toBe(httpRequest.body.email);
+      expect(validatorSpy.password).toBe(httpRequest.body.password);
+      expect(validatorSpy.confirmPassword).toBe(
+        httpRequest.body.confirmPassword,
+      );
+    });
 
-    const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(500);
-    expect(httpResponse.body.error).toBe(new ServerError().message);
+    test('Should return 400 Validator return an error', async () => {
+      const { sut, validatorSpy } = makeSut();
+      jest
+        .spyOn(validatorSpy, 'validate')
+        .mockReturnValueOnce(new Error('any_field').message);
+
+      const httpRequest = {
+        body: {
+          username: 'valid_username',
+          email: 'valid_mail@mail.com',
+          password: '1234',
+          confirmPassword: '1234',
+        },
+      };
+
+      const httpResponse = await sut.handle(httpRequest);
+
+      expect(httpResponse.statusCode).toBe(400);
+      expect(httpResponse.body.error).toBe(
+        new MissingParamError('any_field').message,
+      );
+    });
+
+    test('Should return 500 if no Validator is provided', async () => {
+      const sut = new SignupController();
+      const httpRequest = {
+        body: {
+          username: 'any_username',
+          email: 'any_mail@mail.com',
+          password: '1234',
+          confirmPassword: '1234',
+        },
+      };
+
+      const httpResponse = await sut.handle(httpRequest);
+      expect(httpResponse.statusCode).toBe(500);
+      expect(httpResponse.body.error).toBe(new ServerError().message);
+    });
+
+    test('Should return 500 if Validator has no method create', async () => {
+      class ValidatorSpy {}
+      const validatorSpy = new ValidatorSpy();
+      const sut = new SignupController(validatorSpy);
+      const httpRequest = {
+        body: {
+          username: 'any_username',
+          email: 'any_mail@mail.com',
+          password: '1234',
+          confirmPassword: '1234',
+        },
+      };
+
+      const httpResponse = await sut.handle(httpRequest);
+      expect(httpResponse.statusCode).toBe(500);
+      expect(httpResponse.body.error).toBe(new ServerError().message);
+    });
   });
 });
