8f59c450c456182bd3c49f0db5c7afb36fc52072
Junior Paula <luke.junnior@icloud.com>
2022-11-08 20:17:44 -0300

    move sut creation to a factory method

--- src/utils/validations/require-field-validator.spec.js
+++ src/utils/validations/require-field-validator.spec.js
@@ -11,16 +11,20 @@ class RequireFieldValidator {
   }
 }
 
+const makeSut = () => {
+  return new RequireFieldValidator('field');
+};
+
 describe('RequireFieldValidator', () => {
   test('Should return a MissingParamsError if validation fails', async () => {
-    const sut = new RequireFieldValidator('field');
+    const sut = makeSut();
     const error = sut.validate({ name: 'any_name' });
 
     expect(error).toEqual(new MissingParamError('field').message);
   });
 
   test('Should not return an error if validation succeeds', async () => {
-    const sut = new RequireFieldValidator('field');
+    const sut = makeSut();
     const error = sut.validate({ field: 'any_name' });
 
     expect(error).toBeFalsy();
