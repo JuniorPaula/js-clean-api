abcb59d308eae48dead66f1f814572a7b7e3914b
Junior Paula <luke.junnior@icloud.com>
2022-11-08 19:55:18 -0300

    remove unecessary tests suits

--- src/aplication/controllers/signup/signup.js
+++ src/aplication/controllers/signup/signup.js
@@ -1,6 +1,5 @@
 const {
   MissingParamError,
-  InvalidParamError,
   EmailAlreadyExists,
 } = require('../../../utils/errors');
 const { HttpResponse } = require('../../helpers/http-response');
@@ -20,31 +19,7 @@ class SignupController {
         return HttpResponse.badRequest(new MissingParamError(error));
       }
 
-      const { username, email, password, confirmPassword } = httpRequest.body;
-
-      if (!username) {
-        return HttpResponse.badRequest(new MissingParamError('username'));
-      }
-
-      if (!email) {
-        return HttpResponse.badRequest(new MissingParamError('email'));
-      }
-
-      if (!password) {
-        return HttpResponse.badRequest(new MissingParamError('password'));
-      }
-
-      if (!confirmPassword) {
-        return HttpResponse.badRequest(
-          new MissingParamError('confirmPassword'),
-        );
-      }
-
-      if (password !== confirmPassword) {
-        return HttpResponse.badRequest(
-          new InvalidParamError('confirmPassword'),
-        );
-      }
+      const { username, email, password } = httpRequest.body;
 
       const account = await this.createAccount.create({
         username,
--- src/aplication/controllers/signup/signup.spec.js
+++ src/aplication/controllers/signup/signup.spec.js
@@ -1,6 +1,5 @@
 const {
   MissingParamError,
-  InvalidParamError,
   EmailAlreadyExists,
 } = require('../../../utils/errors');
 const { ServerError } = require('../../errors');
@@ -102,102 +101,6 @@ const makeSut = () => {
 };
 
 describe('Signup Controller', () => {
-  test('Should return 400 if no username is provided', async () => {
-    const { sut } = makeSut();
-
-    const httpRequest = {
-      body: {
-        email: 'any_mail@mail.com',
-        password: '1234',
-        confirmPassword: '1234',
-      },
-    };
-
-    const httpResponse = await sut.handle(httpRequest);
-
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body.error).toBe(
-      new MissingParamError('username').message,
-    );
-  });
-
-  test('Should return 400 if no email is provided', async () => {
-    const { sut } = makeSut();
-
-    const httpRequest = {
-      body: {
-        username: 'any_username',
-        password: '1234',
-        confirmPassword: '1234',
-      },
-    };
-
-    const httpResponse = await sut.handle(httpRequest);
-
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body.error).toBe(
-      new MissingParamError('email').message,
-    );
-  });
-
-  test('Should return 400 if no password is provided', async () => {
-    const { sut } = makeSut();
-
-    const httpRequest = {
-      body: {
-        username: 'any_username',
-        email: 'any_mail@mail.com',
-        confirmPassword: '1234',
-      },
-    };
-
-    const httpResponse = await sut.handle(httpRequest);
-
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body.error).toBe(
-      new MissingParamError('password').message,
-    );
-  });
-
-  test('Should return 400 if no confirmPassword is provided', async () => {
-    const { sut } = makeSut();
-
-    const httpRequest = {
-      body: {
-        username: 'any_username',
-        email: 'any_mail@mail.com',
-        password: '1234',
-      },
-    };
-
-    const httpResponse = await sut.handle(httpRequest);
-
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body.error).toBe(
-      new MissingParamError('confirmPassword').message,
-    );
-  });
-
-  test('Should return 400 if password not match with confirmPassword', async () => {
-    const { sut } = makeSut();
-
-    const httpRequest = {
-      body: {
-        username: 'any_username',
-        email: 'any_mail@mail.com',
-        password: '1234',
-        confirmPassword: '123',
-      },
-    };
-
-    const httpResponse = await sut.handle(httpRequest);
-
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body.error).toBe(
-      new InvalidParamError('confirmPassword').message,
-    );
-  });
-
   test('Should return 500 if no HttpRequest is provided', async () => {
     const { sut } = makeSut();
 
