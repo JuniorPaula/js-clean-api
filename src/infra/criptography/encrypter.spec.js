3e2f830fcad507af51c294c40facd10e5d4c3464
Junior Paula <luke.junnior@icloud.com>
2022-11-10 20:41:38 -0300

    move methods to a single describes

--- src/infra/criptography/encrypter.spec.js
+++ src/infra/criptography/encrypter.spec.js
@@ -25,73 +25,81 @@ const makeSut = () => {
 describe('Encrypter', () => {
   const salt = 12;
 
-  test('Should return true if bcrypt returns true', async () => {
-    const sut = makeSut();
-    const isValid = await sut.compare('any_value', 'hashValue');
+  describe('#compare', () => {
+    test('Should return true if bcrypt.compare returns true', async () => {
+      const sut = makeSut();
+      const isValid = await sut.compare('any_value', 'hashValue');
 
-    expect(isValid).toBe(true);
-  });
-
-  test('Should return false if bcrypt returns false', async () => {
-    const sut = makeSut();
-    bcrypt.isValid = false;
-    const isValid = await sut.compare('any_value', 'hashValue');
-
-    expect(isValid).toBe(false);
-  });
-
-  test('Should call bcrypt with correct values', async () => {
-    const sut = makeSut();
-    await sut.compare('any_value', 'hash');
-
-    expect(bcrypt.value).toBe('any_value');
-    expect(bcrypt.hashValue).toBe('hash');
-  });
-
-  test('Should throws if no params are provided', async () => {
-    const sut = makeSut();
-
-    await expect(sut.compare()).rejects.toThrow(new MissingParamError('value'));
-    await expect(sut.compare('any_value')).rejects.toThrow(
-      new MissingParamError('hash'),
-    );
-  });
-
-  test('Should call bcrypt.hash with correct values', async () => {
-    const sut = makeSut();
-
-    await sut.encrypt('value', salt);
-
-    expect(bcrypt.value).toBe('value');
-    expect(bcrypt.salt).toBe(salt);
-  });
-
-  test('Should throws if no params are provided in encrypt method', async () => {
-    const sut = makeSut();
-
-    await expect(sut.encrypt()).rejects.toThrow(new MissingParamError('value'));
-    await expect(sut.encrypt('any_value')).rejects.toThrow(
-      new MissingParamError('salt'),
-    );
-  });
-
-  test('Should throws if bcrypt.hash throws', async () => {
-    const sut = makeSut();
-
-    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
-      throw new Error();
+      expect(isValid).toBe(true);
     });
 
-    const promise = sut.encrypt('value', salt);
+    test('Should return false if bcrypt.compare returns false', async () => {
+      const sut = makeSut();
+      bcrypt.isValid = false;
+      const isValid = await sut.compare('any_value', 'hashValue');
 
-    await expect(promise).rejects.toThrow();
+      expect(isValid).toBe(false);
+    });
+
+    test('Should call bcrypt.compare with correct values', async () => {
+      const sut = makeSut();
+      await sut.compare('any_value', 'hash');
+
+      expect(bcrypt.value).toBe('any_value');
+      expect(bcrypt.hashValue).toBe('hash');
+    });
+
+    test('Should throws if no params are provided in compare method', async () => {
+      const sut = makeSut();
+
+      await expect(sut.compare()).rejects.toThrow(
+        new MissingParamError('value'),
+      );
+      await expect(sut.compare('any_value')).rejects.toThrow(
+        new MissingParamError('hash'),
+      );
+    });
   });
 
-  test('Should return a valid valueHashed if brcypt.hash succeeds', async () => {
-    const sut = makeSut();
+  describe('#encrypt', () => {
+    test('Should call bcrypt.hash with correct values', async () => {
+      const sut = makeSut();
 
-    const valueHashed = await sut.encrypt('value', salt);
+      await sut.encrypt('value', salt);
 
-    expect(valueHashed).toBe('value_hashed');
+      expect(bcrypt.value).toBe('value');
+      expect(bcrypt.salt).toBe(salt);
+    });
+
+    test('Should throws if no params are provided in encrypt method', async () => {
+      const sut = makeSut();
+
+      await expect(sut.encrypt()).rejects.toThrow(
+        new MissingParamError('value'),
+      );
+      await expect(sut.encrypt('any_value')).rejects.toThrow(
+        new MissingParamError('salt'),
+      );
+    });
+
+    test('Should throws if bcrypt.hash throws', async () => {
+      const sut = makeSut();
+
+      jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+        throw new Error();
+      });
+
+      const promise = sut.encrypt('value', salt);
+
+      await expect(promise).rejects.toThrow();
+    });
+
+    test('Should return a valid valueHashed if brcypt.hash succeeds', async () => {
+      const sut = makeSut();
+
+      const valueHashed = await sut.encrypt('value', salt);
+
+      expect(valueHashed).toBe('value_hashed');
+    });
   });
 });
