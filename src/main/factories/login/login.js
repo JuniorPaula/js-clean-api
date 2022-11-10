ead8abdb0b0cd9b3bc28455b9d6e721e24252b80
Junior Paula <luke.junnior@icloud.com>
2022-11-10 09:20:26 -0300

    inject ValidationComposite into LoginController to validate fields

--- src/main/factories/login/login.js
+++ src/main/factories/login/login.js
@@ -3,7 +3,6 @@ const {
   LoginController,
 } = require('../../../aplication/controllers/login/login');
 const { AuthUsecase } = require('../../../domain/usecases/auth-usecase');
-const { EmailValidator } = require('../../utils/validations/email-validator');
 const {
   LoadUserByEmailRepository,
 } = require('../../../infra/repositories/load-user-by-email-repository');
@@ -15,20 +14,22 @@ const {
   TokenGenerator,
 } = require('../../../infra/criptography/token-generator');
 
+const { loginFactoryValidation } = require('./login-validation-factory');
+
 const makeLoginController = () => {
   const loadUserByEmailRepository = new LoadUserByEmailRepository();
   const encrypter = new Encrypter();
   const tokenGenerator = new TokenGenerator(env.tokeSecret);
   const updateAccessTokenRepository = new UpdateAccessTokenRepository();
 
-  const emailValidator = new EmailValidator();
   const authUsecase = new AuthUsecase(
     loadUserByEmailRepository,
     encrypter,
     tokenGenerator,
     updateAccessTokenRepository,
   );
-  return new LoginController(authUsecase, emailValidator);
+
+  return new LoginController(authUsecase, loginFactoryValidation());
 };
 
 module.exports = { makeLoginController };
--- src/main/routes/login-routes.js
+++ src/main/routes/login-routes.js
@@ -1,5 +1,5 @@
 const { adapterRoutes } = require('../adapters/express-routes-adapter');
-const { makeLoginController } = require('../factories/login');
+const { makeLoginController } = require('../factories/login/login');
 
 const loginRouter = (router) => {
   router.post('/login', adapterRoutes(makeLoginController()));
