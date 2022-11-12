ce425001599ce912cb4e80951f373b7e53dfba28
Junior Paula <luke.junnior@icloud.com>
2022-11-12 14:03:51 -0300

    move tests routes to a owner describes

--- src/main/routes/login-routes.test.js
+++ src/main/routes/login-routes.test.js
@@ -19,41 +19,45 @@ describe('LoginRoutes', () => {
     await MongoHelper.disconnect();
   });
 
-  test('Should return 200 on signup', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        username: 'Jhon Doe',
-        email: 'jhon@mail.com',
-        password: '123456',
-        confirmPassword: '123456',
-      })
-      .expect(200);
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          username: 'Jhon Doe',
+          email: 'jhon@mail.com',
+          password: '123456',
+          confirmPassword: '123456',
+        })
+        .expect(200);
+    });
   });
 
-  test('Should return 200 when valid credentials are provided', async () => {
-    await userModel.insertOne({
-      email: 'jane_email@mail.com',
-      name: 'Jane Doe',
-      password: bcrypt.hashSync('1234', 12),
+  describe('POST /login', () => {
+    test('Should return 200 when valid credentials are provided', async () => {
+      await userModel.insertOne({
+        email: 'jane_email@mail.com',
+        name: 'Jane Doe',
+        password: bcrypt.hashSync('1234', 12),
+      });
+
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'jane_email@mail.com',
+          password: '1234',
+        })
+        .expect(200);
     });
 
-    await request(app)
-      .post('/api/login')
-      .send({
-        email: 'jane_email@mail.com',
-        password: '1234',
-      })
-      .expect(200);
-  });
-
-  test('Should return 401 when invalid credentials are provided', async () => {
-    await request(app)
-      .post('/api/login')
-      .send({
-        email: 'jane_email@mail.com',
-        password: '1234',
-      })
-      .expect(401);
+    test('Should return 401 when invalid credentials are provided', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'jane_email@mail.com',
+          password: '1234',
+        })
+        .expect(401);
+    });
   });
 });
